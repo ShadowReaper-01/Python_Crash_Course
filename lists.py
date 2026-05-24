c517c53f41e99ddd1ff8d78b6c61bc0be7b552bb
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 04:22:27 +0530

    About Lists

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Page No - 35,36 (About Lists)
+fruit_basket = ['apple','mango','banana','grapes','cherry']
+print(fruit_basket[0])
+
+# Page No - 37.38 (About Append,Insert,del in Lists)
+fruit_basket.append('papaya')
+print(fruit_basket)
+fruit_basket.insert(0, 'guava')
+print(fruit_basket)
+del fruit_basket[2]
+print(fruit_basket)
