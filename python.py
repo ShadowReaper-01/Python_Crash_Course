b1c16c48f7f05ab2a07db704b040bf9d5b403271
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 01:35:53 +0530

    Using variables and printing a message

--- python.py
+++ python.py
@@ -1,2 +1,6 @@
 # Page No - 10 (Printing Hello Python World Command)
 print("Hello Python World")
+
+# Page no - 16 (Using Variables and printing a message)
+message = "my name is sparsh"
+print(message)
