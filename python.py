7ec10a1ea168daacb2c0ffcf64e45cb480ad757a
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 04:54:26 +0530

    Ignore This Commit

--- python.py
+++ python.py
@@ -24,7 +24,7 @@ print(para)
 print(paraLstrip)
 print(paraRstrip)
 
-# Page No - 24 (Using removeprefix() and removesuffix() function)
+# Page No - 24 (Using removeprefix() and removesuffix())
 url = 'https://google.com'
 urlRP = url.removeprefix('https://')
 urlRS = url.removesuffix('google.com')
