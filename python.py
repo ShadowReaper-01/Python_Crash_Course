a9014ff90ff52fb43c725bd364c1d5410df9a5cf
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 01:39:01 +0530

    About String and String Methods

--- python.py
+++ python.py
@@ -4,3 +4,9 @@ print("Hello Python World")
 # Page no - 16 (Using Variables and printing a message)
 message = "my name is sparsh"
 print(message)
+
+# Page No - 20 (About String and String Methods)
+name = 'sparsh'
+print(name.title())
+print(name.lower())
+print(name.upper())
