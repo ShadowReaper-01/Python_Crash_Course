06ae22adf216ff64742b2602e3939f6cee73882c
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 15:15:53 +0530

    about Range Function and min , max, sum

--- lists.py
+++ lists.py
@@ -3,3 +3,13 @@ theboyz = ['homelander','stormfront','billy butcher','hughie','ryan','starlight'
 
 for heroes in theboyz:
     print(heroes)
+
+# Page No-52 - 59 (about Range Function and min , max, sum)
+for value in range(0,7):
+    square = value ** 2
+    print(square)
+
+lists = [1,3,5,7,4,7,8,3,6,3,6.10]     
+print(min(lists))
+print(max(lists))
+print(sum(lists))  
