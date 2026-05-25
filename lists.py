9c429d48b73a028d4b28770a8d3d48d8673ec2d1
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 15:36:36 +0530

    about tuples

--- lists.py
+++ lists.py
@@ -4,7 +4,7 @@ theboyz = ['homelander','stormfront','billy butcher','hughie','ryan','starlight'
 for heroes in theboyz:
     print(heroes)
 
-# Page No-52 - 59 (about Range Function and min , max, sum)
+# Page No - 52 - 59 (about Range Function and min , max, sum)
 for value in range(0,7):
     square = value ** 2
     print(square)
@@ -14,9 +14,16 @@ print(min(lists))
 print(max(lists))
 print(sum(lists))  
 
-# Page no 60 - 65 (about list comprehension and lists slicing)
+# Page no - 60 - 65 (about list comprehension and lists slicing)
 newlist = ['apple', 'mango','banana','grapes','cherry']
 fruits = [fruit for fruit in newlist]
 print(fruits)
 print(newlist[0:5])
 print(newlist[-1:-3])
+
+# Page No - 66,67 (about tuples)
+
+tuple = (39 , 40 , 41)
+print(tuple)
+tuple = (43,43,45)
+print(tuple)
