0a15d63ba815fa8bf50daf55bcc86ab4c2e9e812
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 15:28:31 +0530

    about list comprehension and lists slicing

--- lists.py
+++ lists.py
@@ -13,3 +13,10 @@ lists = [1,3,5,7,4,7,8,3,6,3,6.10]
 print(min(lists))
 print(max(lists))
 print(sum(lists))  
+
+# Page no 60 - 65 (about list comprehension and lists slicing)
+newlist = ['apple', 'mango','banana','grapes','cherry']
+fruits = [fruit for fruit in newlist]
+print(fruits)
+print(newlist[0:5])
+print(newlist[-1:-3])
