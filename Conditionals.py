df2f7f1c63aa5dc0cd5d953bd2f761de6c694d8a
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-25 18:30:24 +0530

    if else statements

--- Conditionals.py
+++ Conditionals.py
@@ -6,3 +6,10 @@ for numbers in lists:
         print(f"numbers are even {numbers}")
         count += 1
         print(f"numbers that are even {count}")
+
+# Page No - 79 (if else statements)
+age = int(input())
+if age < 18:
+    print("you are not adult enough to see this movie!!")
+else:
+    print("you are adult enough to see this movie")    
