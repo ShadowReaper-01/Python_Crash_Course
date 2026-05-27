7880711e7bd8510dbffbed3c07b4ced2f7289d25
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-27 14:33:26 +0530

    Sorting A List in A loop

--- dictionaries.py
+++ dictionaries.py
@@ -21,3 +21,12 @@ del(dict1['ranking'])
 print(dict1)
 print(dict1.get("xyz key"))
 
+# Page No- 99,100 (Looping Through a Dictionary)
+for key,value in dict1.items():
+    print(f"{key} this is the key and its value is {value}")
+
+# Page No- 100,104 (sorting a list in a loop)
+dict2 = {'student1':"aman",'student2':'riya','student3':'kunal','student4':'zoya'}
+for value in sorted(dict2.values()):   
+    print(f"these are the sorted values of dictionary 2 \n {value}") 
+
