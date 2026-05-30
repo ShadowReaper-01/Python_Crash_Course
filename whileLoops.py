5a9bc26f40b53a9980d5eee8b9decda8695476fe
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-30 16:50:02 +0530

    User Input Function

---
+++ whileLoops.py
@@ -0,0 +1,8 @@
+#Page No-114,115,116,117 (About using Input Function)
+user_input = int(input("enter your number = "))
+
+if user_input %10 ==0:
+    print("numbers is divisible by 10!!")
+
+else:
+    print("number is not divisible by 10!!")    
