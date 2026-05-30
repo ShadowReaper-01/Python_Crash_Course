d561f7ba0f260e754447708de4b648e0053d2e47
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-30 18:57:51 +0530

    Introduction to While Loops

--- whileLoops.py
+++ whileLoops.py
@@ -5,4 +5,15 @@ if user_input %10 ==0:
     print("numbers is divisible by 10!!")
 
 else:
-    print("number is not divisible by 10!!")    
+    print("number is not divisible by 10!!") 
+
+# Page no - 118,119 (Inroduction Of While Loops)
+number = 2
+while number < 20:
+    number += 1
+    print(number)
+
+message = input("Who is your favourite superhero?")
+
+while message != 'quit':
+    print(f"{message}, that is also mine favourite superhero!!")
