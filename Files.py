6e387c595ada5c7dc8a41991b0cec5e0059eeb8c
ShadowReaper-01 <gautam.sparsh4321@gmail.com>
2026-05-31 19:53:57 +0530

    Files and Handling Exceptions

---
+++ Files.py
@@ -0,0 +1,18 @@
+with open(r"C:\Users\gauta\Desktop\pivalue.txt", "r",encoding = 'utf-8') as file:
+    content = file.read()
+    
+    print(content)
+
+    lines = content.splitlines()
+    print(lines)
+    print(len(content))
+
+
+try:
+    print(5/0)
+    pass
+
+except:
+    print("man! you cant devide by 0!!")        
+ 
+   
